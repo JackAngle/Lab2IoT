dd03a4dd0ca265e2a9b52a42c87d296d4a526944
JackAngle <43754974+JackAngle@users.noreply.github.com>
2018-10-31 20:53:00 +0700

    new commit

---
+++ app/src/main/java/com/example/lehuy/thesis_server/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.lehuy.thesis_server;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+import cc.mvdan.accesspoint.WifiApControl;
+
+/**
+ * Skeleton of an Android Things activity.
+ * <p>
+ * Android Things peripheral APIs are accessible through the class
+ * PeripheralManagerService. For example, the snippet below will open a GPIO pin and
+ * set it to HIGH:
+ * <p>
+ * <pre>{@code
+ * PeripheralManagerService service = new PeripheralManagerService();
+ * mLedGpio = service.openGpio("BCM6");
+ * mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+ * mLedGpio.setValue(true);
+ * }</pre>
+ * <p>
+ * For more complex peripherals, look for an existing user-space driver, or implement one if none
+ * is available.
+ *
+ * @see <a href="https://github.com/androidthings/contrib-drivers#readme">https://github.com/androidthings/contrib-drivers#readme</a>
+ */
+public class MainActivity extends Activity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    WifiManager mWifiManager = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mWifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+
+        WifiConfiguration wifiConfiguration = new WifiConfiguration();
+        wifiConfiguration.SSID = "LeHuyThinh156";
+        wifiConfiguration.preSharedKey = "12345687";
+        wifiConfiguration.hiddenSSID = false;
+        wifiConfiguration.status = WifiConfiguration.Status.ENABLED;
+        wifiConfiguration.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
+        wifiConfiguration.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
+        wifiConfiguration.allowedKeyManagement.set(4);
+        wifiConfiguration.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
+        wifiConfiguration.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
+        /*
+        try{
+            Method method = mWifiManager.getClass().getMethod("setWifiApEnabled", WifiConfiguration.class, boolean.class);
+            method.invoke(mWifiManager, wifiConfiguration, true);
+        }catch (Exception e){
+            Log.i(TAG, "Can't create AP");
+            e.printStackTrace();
+
+        }
+        */
+        WifiApControl apControl = WifiApControl.getInstance(this);
+
+        apControl.setEnabled(wifiConfiguration, true);
+    }
+}
